306810b2747e1b06ec72b4c8d1d501fb5914e90c
Caolán McNamara <caolanm@redhat.com>
2011-09-26 09:40:30 +0100

    fix ukrainian

--- src/test.sh
+++ src/test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd ../langclass/LM
-res=`cat ../ShortTexts/$1.txt | ../../src/testtextcat ../fpdb.conf`
+#take first guess
+res=`cat ../ShortTexts/$1.txt | ../../src/testtextcat ../fpdb.conf | sed -e "s/].*/]/"`
 if [ $res == "[$1--utf8]" ]; then
     exit 0
 else
